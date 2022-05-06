c4dec15e7478ea924a68a8f613058c1b0ab27d94
Pedro Ecsedei <96201154+EcsedeiPedro@users.noreply.github.com>
2022-05-05 21:22:14 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,54 +1,12 @@
-const form = document.getElementById("form");
-const username = document.getElementById("username");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const passwordConfirmation = document.getElementById("password-confirmation");
-const enviar = document.getElementById("enviar");
+const formulario = document.getElementById('form').value;
+const usuario = document.getElementById('username').value;
+const eMail = document.getElementById('email').value;
+const senha = document.getElementById('password').value;
+const senhaConfirma = document.getElementById('passwordConfirmation').value;
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  checkInputs();
-});
-
-function checkInputs() {
-  const usernameValue = username.value;
-  const emailValue = email.value;
-  const passwordValue = password.value;
-  const passwordConfirmationValue = passwordConfirmation.value;
-
-  if (usernameValue === "") {
-    setErrorFor(username, "O nome de usuário é obrigatório.");
-  } else if (usernameValue.length < 7) {
-    setErrorFor(
-      username,
-      "O nome de usuário precisa ter no mínimo 7 caracteres."
-    );
-  } else {
-    setSuccessFor(username);
-  }
-
-  if (emailValue === "") {
-    setErrorFor(email, "O email é obrigatório.");
-  } else if (!checkEmail(emailValue)) {
-    setErrorFor(email, "Por favor, insira um email válido");
-  } else {
-    setSuccessFor(email);
-  }
-
-  if (passwordValue === "") {
-    setErrorFor(password, "A senha é obrigatória.");
-  } else if (passwordValue.length < 7) {
-    setErrorFor(password, "A senha precisa ter no mínimo 7 caracteres.");
-  } else {
-    setSuccessFor(password)
-  }
-
-  if (passwordConfirmationValue === "") {
-    setErrorFor(passwordConfirmation, "A confirmação de senha é obrigatória.");
-  } else if (passwordConfirmationValue !== passwordValue) {
-    setErrorFor(passwordConfirmation, "As senhas não conferem");
-  } else {
-    setSuccessFor(passwordConfirmation);
+function usuario() {
+  if () {
+    
   }
 }
 
@@ -70,8 +28,4 @@ function setSuccessFor(input) {
   formControl.className = "form-control success";
 }
 
-function checkEmail(email) {
-  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-    email
-  );
-}
+
